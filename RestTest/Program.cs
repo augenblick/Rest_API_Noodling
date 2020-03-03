6cf8a1f4bb3cf9c30db8a824a62f8cf41fb6540c
James Grubbs <jngrubbs@uno.edu>
2020-03-03 09:32:31 -0600

    moving git repo up to parent directory

---
+++ RestTest/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace RestTest
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Debug.WriteLine("TEST");
+            TraceListenerCollection listeners = Debug.Listeners;
+            foreach (TraceListener listener in listeners) {
+                Console.WriteLine(listener.Attributes);
+            }
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new FormRestTest());
+        }
+    }
+}
